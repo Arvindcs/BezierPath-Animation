8d840e6d902fbb0b2d3e43d045a0494ae38cff90
Arvind <Arvindkumar7161@gmail.com>
2021-02-05 15:39:54 +0530

    added CGAffineTransform

--- CarAnimation/CarAnimation/View/ViewController.swift
+++ CarAnimation/CarAnimation/View/ViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var carView: CarView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //TODO:- change watch size as per your requirement
+        //self.carView.transform = CGAffineTransform(scaleX: 0.8, y: 0.28) // Orientation Horizontal iPhone
     }
 }
